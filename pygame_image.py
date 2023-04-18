db9cf0b79052d84af8ee46e2e357f8d9209c4256
fish <c0b22092ca@edu.teu.ac.jp>
2023-04-18 15:39:16 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -6,9 +6,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #Surface生成
-    kk_img = pg.image.load("ex01/fig/3.png") #こうかとん生成
-    kk_img = pg.transform.flip(kk_img, True, False) #左右反転
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0) #10度反時計回りに回転
+    kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False) #こうかとん生成
+    list = [kk_img, pg.transform.flip(kk_img, 10, 1.0)]
 
 
 
